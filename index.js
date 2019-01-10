f2bef6078ce083e4f9cdeb5a542cd5ab4cb64518
nicole <scsfmac11@Admins-MacBook-Air.local>
2019-01-09 16:00:12 -0800

    added updates + bg colors

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express=require("express")
 const app = express()
 const ejs = require('ejs')
 
+app.use(express.static(__dirname + '/public'));
+
 app.set("view engine", "ejs")
 
 app.get('/', (req, res)=> {
